844eb41e9017b61251bb65ffd63c25b42c004894
shoomilas <jakub.szumilas@gmail.com>
2021-05-24 03:39:58 +0200

    Server routes config

---
+++ server/api/routes/api.py
@@ -0,0 +1,4 @@
+from fastapi import APIRouter
+from app.api.routes import sample
+router = APIRouter()
+router.include_router(sample.router, tags=["sample"], prefix="/sample")
--- server/main.py
+++ server/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+# from app.api.routes.api import router as api_router
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.post("/api/compute/")
+def create_computation(to_compute):
+    return { "result": f"{to_compute}"}
