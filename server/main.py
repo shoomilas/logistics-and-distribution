6e1cfb2188b46af63fbd156f084f455124d72bee
shoomilas <jakub.szumilas@gmail.com>
2021-05-30 02:45:18 +0200

    Proper solving with PuLP

--- server/main.py
+++ server/main.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import Optional
 from fastapi.middleware.cors import CORSMiddleware
+from pulp import LpMaximize, LpProblem, LpStatus, lpSum, LpVariable
 
 import math
 from scipy.optimize import linprog
@@ -43,46 +44,104 @@ def read_root():
 def create_distribution(distribution_data: distribution_data):
     print(distribution_data.__dict__)
     print("========")
-    result = compute_result(distribution_data)
-    # return {"input": distribution_data.__dict__, "result":result}
+    result = compute_result(distribution_data) 
     return {"input": distribution_data.__dict__, "result": distribution_data.__dict__}
     
 @app.post("/api/distribution2/")
 def create_distribution(distribution_data: distribution_data):
     print(distribution_data.__dict__)
     print("========")
-    result = compute_result2(distribution_data)
+    # result = compute_result2(distribution_data)
+    result = compute_result_pulp(distribution_data)
     return {"input": distribution_data.__dict__, "result": result}
+
+# def compute_result_pulp(distribution_data):
+
+
+def compute_result_pulp(distribution_data):
+    model = LpProblem(name="resource-allocation", sense=LpMaximize)
+    # products_len = len(distribution_data.gains) # number of products
+    # x = {i: LpVariable(name=f"x{i}", lowBound=0) for i in range(0, products_len)}
+    # materials_len = len(distribution_data.constraints)
+    # for i in range (0,materials_len):
+    #     model += lpSum( distribution_data.values[]  )
+    #     model += lpSum( x[0] * distribution_data.values[i][0] )
+
+    #############################
+    # x = {i: LpVariable(name=f"x{i}", lowBound=0) for i in range(1, 4)}
+    # x[1].upBound=200
+    # x[2].upBound=120
+    # x[3].upBound=60
+    # model += (5*x[1] + 3*x[2] + 1 * x[3] <= 3600, "Mat 1")
+    # model += (1*x[1] + 2*x[2] + 4 * x[3] <= 4800, "Mat 2")
+    # model += 1800*x[1] + 2400 * x[2] + 3000 * x[3]
+    #############################
+    x = {i: LpVariable(name=f"x{i}", lowBound=0) for i in range(1, 3)}
+    model += (2*x[1] + 1*x[2] <= 1000, "Mat 1")
+    model += (3*x[1] + 3*x[2] <= 2400, "Mat 2")
+    model += (1.5*x[1] + 2*x[2] <= 600, "Mat 3")
+    model += 15*x[1] + 20 * x[2] 
+    
+    
+    # Solve the optimization problem
+    status = model.solve()
+    # Get the results
+    print(f"status: {model.status}, {LpStatus[model.status]}")
+    print(f"objective: {model.objective.value()}")
+
+    for var in x.values():
+        print(f"{var.name}: {var.value()}")
+
+    for name, constraint in model.constraints.items():
+        print(f"{name}: {constraint.value()}")
+
+    result = f"{model.solver}"
+    print(result)
+    return {'res': result}
 
 def define_bounds(min_bounds, max_bounds):
     bounds = []
     for bound in zip(min_bounds,max_bounds):
         min = bound[0]
         max = bound[1]
+        # if(min==None): min = -math.inf
+        # if(max==None): max = math.inf
+        
+        # if(min==None): min = -math.inf
+        # if(max==None): max = 0
+
         if(min==None): min = 0
         if(max==None): max = math.inf
-        bound=(min,max)
+
+        # bound=(min,max)
+        bound=(max,min)
         bounds.append(bound)
     return bounds
 
-def compute_result2(distribution_data: distribution_data): # original but it minimizes
+def compute_result2(distribution_data: distribution_data): # Trying to maximize
     defined_bounds = define_bounds(distribution_data.pieces_min, distribution_data.pieces_max)
+    gains = [-x for x in distribution_data.gains]
+    constraints = [-x for x in distribution_data.constraints]
+    constraints = distribution_data.constraints
+
     print(f"VALUES: {distribution_data.values}")
-    print(f"CONSTRAINTS: {distribution_data.constraints}")
+    print(f"CONSTRAINTS: {constraints}")
     print(f"BOUNDS: {defined_bounds}")
-    
+    print(f"GAINS: {gains}")
+
     opt = linprog(
-        c=distribution_data.gains,
+        # c =  np.transpose(gains),     
+        c = gains,
         A_ub=distribution_data.values,
-        b_ub=distribution_data.constraints,
+        b_ub=constraints,
         bounds = defined_bounds,
-        method="revised simplex"
+        method="revised simplex",
+        options={"disp": True}
     ) 
 
     result = f"{opt}"
     print(result)
-    return {'fun': opt['fun'], "msg": opt['message'], "x": list(opt['x'])}
-
+    return {'fun': -opt['fun'], "msg": opt['message'], "x": list(opt['x'])}
 
 def compute_result(distribution_data: distribution_data): # original but it minimizes
     defined_bounds = define_bounds(distribution_data.pieces_min, distribution_data.pieces_max)
@@ -91,11 +150,11 @@ def compute_result(distribution_data: distribution_data): # original but it mini
     print(f"BOUNDS: {defined_bounds}")
     
     opt = linprog(
-        c=distribution_data.gains,
+        c= distribution_data.gains,   #
         A_ub=distribution_data.values,
         b_ub=distribution_data.constraints,
         bounds = defined_bounds,
-        method="revised simplex"
+        method="revised simplex",
     ) 
 
     result = f"{opt}"
@@ -117,15 +176,4 @@ def compute_result(distribution_data: distribution_data): # original but it mini
     #         (120, math.inf),
     #         (60, math.inf)
     #     ],
-    #     method="simplex"
-
-    # valuies             constraints           gains                pieces
-    # tabelka_wartosc, limity_wartosci, zyski_wartosci, ograniczenia_wartosci
-    # lhs_ineq,       rhs_ineq,           obj,              ograniczenia
-    # opt = linprog(c=obj, A_ub=lhs_ineq, b_ub=rhs_ineq, bounds=bnd, method="simplex") 
-    # opt = linprog(c=distribution_data.gains, 
-    #     A_ub=distribution_data.constraints,
-    #     b_ub=distribution_data.constraints,
-    #     bounds=distribution_data.pieces,
-    #     method="simplex"
-    # ) 
+    #     method="simplex
