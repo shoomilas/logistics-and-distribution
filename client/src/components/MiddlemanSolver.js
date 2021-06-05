f9ef4925ae6c301c21fa85a7812c9514e1ad9853
shoomilas <jakub.szumilas@gmail.com>
2021-06-05 17:03:06 +0200

    Update MiddlemanSolver.js

--- client/src/components/MiddlemanSolver.js
+++ client/src/components/MiddlemanSolver.js
@@ -8,48 +8,55 @@ const MiddlemanSolver = () => {
     const rowsInputRef = useRef();
     const colsInputRef = useRef();
     const [solverInput, setSolverInput] = useState(<div></div>);    
-    const [outcomeComponent, setOutcomeComponent] = useState(<div></div>);    
-    const [dataArray, setDataArray] = useState()
+    const [outcomeComponent, setOutcomeComponent] = useState(<div></div>);
+    const [singleCostArray, setSingleCostArray] = useState()
+    // const [suppliersArray, setSuppliersArray] = useState([])
     
     let nRows = 4
     let nCols = 4
     let nProfitAtTheEnd, nExpense
 
     console.log("[MiddlemanSolver.js: SingleCostsTable]");
-    console.log(dataArray);
+    console.log(singleCostArray);
 
     const prepOutcome = (event) => {
         // TODO: Why is it not able to access dateArray from inside of this function????
-        console.log("[SOLVE CLICKED] DATA ARRAY:"); 
-        console.log(dataArray);
+        console.log("[SOLVE CLICKED] singleCostArray:"); 
+        console.log(singleCostArray);
         const returnedElem = () => (<div>
-            {/* <h3>{dataArray[0][0]}</h3> */}
-            <h2>Y</h2>
+            {/* <h3>{singleCostArray[0][0]}</h3> */}
+            <h2>Why is it not able to access dateArray from inside of this function????</h2>
         </div>)
         setOutcomeComponent(returnedElem)
     }
 
     const prepInputForms = (event) => {
         event.preventDefault(); 
+        let suppliersArr = []
         const refreshSingleCosts = (outputData) => {
             // console.log("[OUTPUT FROM DYNAMIC TABLE]");
             // console.log(outputData);
-            setDataArray(outputData)
+            setSingleCostArray(outputData)
         }
-        setDataArray(Array.from({length: rowsInputRef.current.value},() => Array.from({length: colsInputRef.current.value}, () => 0)))
+        setSingleCostArray(Array.from({length: rowsInputRef.current.value},() => Array.from({length: colsInputRef.current.value}, () => 0)))
         const SuppliersInput = () => (<>{_.range(0,rowsInputRef.current.value).map((x) => <div>
                 <form>
                     <p><b>Supplier #{x}</b></p>
-                    <label>Supply:</label><input   type="number" name="rows" defaultValue="3" min="1" max ="40"/>
-                    <label>Cost:</label><input     type="number" name="rows" defaultValue="3" min="1" max ="40"/>
+                    <label>Supply:</label>
+                    <input type="number" name="supply" defaultValue="0" min="0" onChange={ (e) => {
+                        suppliersArr[x] = e.value
+                        console.log(`Setting suppliersArr[${x}] as ${e.currentTarget.value}`)
+                    } }/>
+                    <label>Cost:</label>
+                    <input type="number" name="cost" defaultValue="0" min="0"/>
                 </form>
             </div>)}</>)
         
         const BuyersInput = () => (<>{_.range(0,colsInputRef.current.value).map((x) => <div>
             <form>
                 <p><b>Buyer #{x}</b></p>
-                <label>Demand:</label><input   type="number" name="rows" defaultValue="3" min="1" max ="40"/>
-                <label>Price:</label><input  type="number" name="rows" defaultValue="3" min="1" max ="40"/>
+                <label>Demand:</label><input   type="number" name="demand" defaultValue="0" min="1"  />
+                <label>Price:</label><input  type="number" name="price" defaultValue="0" min="1"  />
             </form>
         </div>)}</>)
 
